75f684d761e970abddc7bde2b9546257120ab770
mahendraverma2019 <52949479+mahendraverma2019@users.noreply.github.com>
2021-09-29 20:48:10 +0530

    updated pojo

--- MovieCatalogService/src/main/java/com/example/demo/CatalogItem.java
+++ MovieCatalogService/src/main/java/com/example/demo/CatalogItem.java
@@ -4,13 +4,23 @@ public class CatalogItem {
 private String title;
 private String description;
 private int rating;
+private String overview;
+private double imdbRating;
 
-
-public CatalogItem(String title, String description, int rating) {
+public CatalogItem(String title, String name, int rating){
 	super();
 	this.title = title;
 	this.description = description;
 	this.rating = rating;
+}
+
+public CatalogItem(String title, String description, int rating, String overview, double imdbRating) {
+	super();
+	this.title = title;
+	this.description = description;
+	this.rating = rating;
+	this.overview = overview;
+	this.imdbRating = imdbRating;
 }
 
 public String getTitle() {
@@ -31,6 +41,19 @@ public int getRating() {
 public void setRating(int rating) {
 	this.rating = rating;
 }
+	public String getOverview() {
+		return overview;
+	}
 
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
 
+	public double getImdbRating() {
+		return imdbRating;
+	}
+
+	public void setImdbRating(double imdbRating) {
+		this.imdbRating = imdbRating;
+	}
 }
