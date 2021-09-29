ad2125211c319725e1e7df1327fe9daad655ef55
mahendraverma2019 <52949479+mahendraverma2019@users.noreply.github.com>
2021-09-29 20:48:34 +0530

    updated pojo

--- MovieCatalogService/src/main/java/com/example/demo/Movie.java
+++ MovieCatalogService/src/main/java/com/example/demo/Movie.java
@@ -3,12 +3,16 @@ package com.example.demo;
 public class Movie {
 private String movieId;
 private String name;
+private String overview;
+private double rating;
 
 public Movie() {}
-public Movie(String movieId, String name) {
+public Movie(String movieId, String name, String overview, double rating) {
 	super();
 	this.movieId = movieId;
 	this.name = name;
+	this.overview = overview;
+	this.rating = rating;
 }
 public String getMovieId() {
 	return movieId;
@@ -23,4 +27,19 @@ public void setName(String name) {
 	this.name = name;
 }
 
+	public String getOverview() {
+		return overview;
+	}
+
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
 }
