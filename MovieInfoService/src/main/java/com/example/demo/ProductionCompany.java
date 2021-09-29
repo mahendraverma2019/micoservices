c1dcecde3a439311dcedfb2729c8a76b5c44a17d
mahendraverma2019 <52949479+mahendraverma2019@users.noreply.github.com>
2021-09-29 20:51:24 +0530

    added refactored pojo files

---
+++ MovieInfoService/src/main/java/com/example/demo/ProductionCompany.java
@@ -0,0 +1,84 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "id",
+        "logo_path",
+        "name",
+        "origin_country"
+})
+@Generated("jsonschema2pojo")
+public class ProductionCompany {
+
+    @JsonProperty("id")
+    private int id;
+    @JsonProperty("logo_path")
+    private String logoPath;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("origin_country")
+    private String originCountry;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("id")
+    public int getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @JsonProperty("logo_path")
+    public String getLogoPath() {
+        return logoPath;
+    }
+
+    @JsonProperty("logo_path")
+    public void setLogoPath(String logoPath) {
+        this.logoPath = logoPath;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("origin_country")
+    public String getOriginCountry() {
+        return originCountry;
+    }
+
+    @JsonProperty("origin_country")
+    public void setOriginCountry(String originCountry) {
+        this.originCountry = originCountry;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
